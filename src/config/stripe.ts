d000170afcada130b501efda1a009e4b39b49632
iasadk <89456039+iasadk@users.noreply.github.com>
2024-04-06 07:28:36 +0530

    Completed checkout process.

---
+++ src/config/stripe.ts
@@ -0,0 +1,28 @@
+export const PLANS = [
+  {
+    name: "Free",
+    slug: "free",
+    quota: 10,
+    pagesPerPdf: 5,
+    price: {
+      amount: 0,
+      priceIds: {
+        test: "",
+        production: "",
+      },
+    },
+  },
+  {
+    name: "Pro",
+    slug: "pro",
+    quota: 30,
+    pagesPerPdf: 30,
+    price: {
+      amount: 14,
+      priceIds: {
+        test: "price_1P2H5JSJXai2bfNFL1JatdpW",
+        production: "",
+      },
+    },
+  },
+];
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -4,6 +4,9 @@ import { TRPCError } from "@trpc/server";
 import { db } from "@/db";
 import { z } from "zod";
 import { INFINITE_QUERY_LIMIT } from "@/config/infinite-query";
+import { absoluteUrl } from "@/lib/utils";
+import { getUserSubscriptionPlan, stripe } from "@/lib/stripe";
+import { PLANS } from "@/config/stripe";
 export const appRouter = router({
   //   test:publicProcedure.query(()=>{
   //     return "OK..Working"
@@ -134,18 +137,62 @@ export const appRouter = router({
         },
       });
 
-      let nextCursor : typeof cursor | undefined = undefined;
-      if(messages.length > limit){
-        const nextItem =  messages.pop();
+      let nextCursor: typeof cursor | undefined = undefined;
+      if (messages.length > limit) {
+        const nextItem = messages.pop();
         nextCursor = nextItem?.id;
       }
 
       return {
         messages,
-        nextCursor
+        nextCursor,
       };
-    })
+    }),
 
+  createStripeSession: privateProcedure.mutation(async ({ ctx }) => {
+    const { userId } = ctx;
+    const billingUrl = absoluteUrl("/dashboard/billing");
+    if (!userId) throw new TRPCError({ code: "UNAUTHORIZED" });
+
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: userId,
+      },
+    });
+    if (!dbUser) throw new TRPCError({ code: "UNAUTHORIZED" });
+
+    // If already sub or not:
+    // if yes then the can manage substription
+    const subscriptionPlan = await getUserSubscriptionPlan();
+    if (subscriptionPlan.isSubscribed && dbUser.stripeCustomerId) {
+      const stripeSession = await stripe.billingPortal.sessions.create({
+        customer: dbUser.stripeCustomerId,
+        return_url: billingUrl,
+      });
+      return {
+        url: stripeSession.url,
+      };
+    }
+    // else the can purchase plan:
+    const stripeSession = await stripe.checkout.sessions.create({
+      success_url: billingUrl,
+      cancel_url: billingUrl,
+      payment_method_types: ["card"],
+      mode: "subscription",
+      billing_address_collection: "required",
+      line_items: [
+        {
+          price: PLANS.find((x) => x.name === "Pro")?.price.priceIds.test,
+          quantity: 1,
+        },
+      ],
+      metadata: {
+        userId: userId,
+      },
+    });
+
+    return { url: stripeSession.url };
+  }),
 });
 // Export type router type signature,
 // NOT the router itself.

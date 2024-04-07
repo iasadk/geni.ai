663250c904a7c0e96c9a3a85755bf20bc703aaf3
iasadk <89456039+iasadk@users.noreply.github.com>
2024-04-07 20:11:22 +0530

    Added Prod URL.

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -9,7 +9,7 @@ const Providers = ({ children }: PropsWithChildren) => {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
+          url: `${process.env.PROD_URL}/api/trpc`,
         }),
       ],
     })
